49b7c3bf743b76492cdf2ca0b9b5de657d819329
W. Clark Newell <clark@clarknewell.tech>
2019-04-18 16:24:24 -0600

    front end mostly complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +7,4 @@ import { Post } from './posts/post.model';
 })
 export class AppComponent {
   title = 'max-mean';
-  storedPosts: Post[] = [];
-
-  onPostAdded(post) {
-    this.storedPosts.push(post);
-  }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model'
 import { PostsService } from '../posts.service';
@@ -8,7 +9,7 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
 
   // posts = [
   //   { title: "Fist Post", content: "This is the first post" },
@@ -16,7 +17,21 @@ export class PostListComponent {
   //   { title: "Third Post", content: "This is the third post" }
   // ];
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+ ngOnDestroy() {
+  this.postsSub.unsubscribe();
+ }
+
 }
